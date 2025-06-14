9a76368fbc0a414f16333cba00acd07f80518e98
dengxiongfeng <dengxiongfeng@baidu.com>
2025-06-14 15:47:26 +0800

    feat: :sparkles: 更新 Koa 服务器端口配置，支持环境变量

--- packages/docserver/src/index.js
+++ packages/docserver/src/index.js
@@ -1,9 +1,11 @@
 import Koa from 'koa';
 const app = new Koa();
-
+const PORT = process.env.PORT || 3000;
 // response
 app.use(ctx => {
   ctx.body = 'Hello Koa';
 });
 
-app.listen(3001);
+app.listen(PORT, () => {
+  console.log(`✅Server is running on http://localhost:${PORT}`);
+});
